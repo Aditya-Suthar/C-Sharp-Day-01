ff4b69a5d4a0740837af8e431dbdd124c40de30e
Aditya Suthar <adityabishnoi576@gmail.com>
2025-09-08 16:48:06 +0530

    Hello

---
+++ LET US C QUESTION ANSWERS CHAPTER 1/Practice.c
@@ -0,0 +1,18 @@
+// Demonstration of if statement
+
+#include <stdio.h>
+
+int main ()
+
+{
+    int a ;
+
+    printf ("Enter a number less than 10 :") ;
+    scanf("%d" , &a) ;
+
+    if (a <= 10) 
+        printf ("What an obedient servant you are !") ;
+
+return 0 ;
+
+}
