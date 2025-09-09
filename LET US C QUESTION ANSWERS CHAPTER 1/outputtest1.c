ab4b09147ea9c7e51cc3dff917e7ae443997511f
Aditya Suthar <adityabishnoi576@gmail.com>
2025-09-09 22:46:10 +0530

    else ifs

---
+++ LET US C QUESTION ANSWERS CHAPTER 1/outputtest1.c
@@ -0,0 +1,28 @@
+//If cost price and selling price of an item is input through the keyboard, write a program
+//to determine wheter the seller has made profit or incurred loss.Also determine how much profit
+//he made or loss he incurred.
+
+#include <stdio.h>
+int main (void)
+
+{
+    int sp,cp,a;
+
+    printf("Enter the Selling price and Cost Price of object respectively");
+    scanf ("%d %d" ,&sp,&cp);
+
+    a = sp-cp ;
+
+    if (a>0)
+        printf ("This is a profit of %d" , a);
+    
+    else {
+    if (a<0)
+        printf ("This is a loss of %d", -a);
+    else
+        printf ("No loss nor any profit");
+    }
+    
+    return 0;
+    
+}
