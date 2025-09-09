a0ba810da8d88ed519122ac13c8f3f72b22672b7
Aditya Suthar <adityabishnoi576@gmail.com>
2025-09-09 23:04:37 +0530

    leap year or not

---
+++ LET US C QUESTION ANSWERS CHAPTER 1/outputtest3.c
@@ -0,0 +1,19 @@
+//Any year is input throught the keyboard.Write a program to determine wheter the 
+//year is a leap year or not
+
+#include <stdio.h>
+#include <conio.h>
+int main (void)
+
+{
+    int a ;
+    
+    if (a%4==0)
+        print ("The year is the leap year");
+    else 
+        printf ("The year is not the leap year");
+
+    getch();
+    return 0;
+
+}
