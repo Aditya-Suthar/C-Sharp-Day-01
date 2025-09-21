3cdb944b4bf5fc84ac6d7daa0bb9dcd776630f6e
Aditya Suthar <adityabishnoi576@gmail.com>
2025-09-21 17:28:24 +0530

    Pattern

---
+++ LET US C QUESTION ANSWERS CHAPTER 1/Pattern.c
@@ -0,0 +1,20 @@
+//input numers for design of stair type drawings
+
+#include <stdio.h>
+int main ()
+{
+    int row,col,n,i=0;
+    
+    printf ("Enter the value of n");
+    scanf ("%d",&n);
+
+    for (row=1;row<=n;row++){
+        for (col=1;col<=n;col++)
+        {
+            i = i+1;
+            printf ("%d",i);
+        }
+        printf("\n");
+    }
+
+}
