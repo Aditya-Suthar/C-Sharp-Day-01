148d5b95f295ce6b41b3de0ee751a34bb81d2fa4
Aditya Suthar <adityabishnoi576@gmail.com>
2025-12-16 13:06:21 +0530

    Volume calculation using c++

---
+++ C++/volumeofsphere.cpp
@@ -0,0 +1,19 @@
+// volume of sphere
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+    float p = 3.14;
+    int r = 5;
+
+    float v;
+
+    v = 4.0/3 * p * r * r * r;
+
+    cout << v << "\n";
+
+}
