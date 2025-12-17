0cf27b7bd03a9307679518743b7a4c0a9576884e
Aditya Suthar <adityabishnoi576@gmail.com>
2025-12-17 12:41:55 +0530

    Checking odd even numebrs

---
+++ C++/InputOutput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+    int x;
+    cout << "Enter value of x :" ;
+    cin >> x;
+
+    int y;
+    cout << "Enter value of y :";
+    cin >> y;
+
+    cout << "Product of numbers is :" << x*y;
+    
+} 
