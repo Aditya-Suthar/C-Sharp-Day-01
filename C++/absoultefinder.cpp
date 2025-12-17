9d982ece09b75c781fca41d9f46a45fff2a3b37b
Aditya Suthar <adityabishnoi576@gmail.com>
2025-12-17 12:59:11 +0530

    printing absolute numbers

---
+++ C++/absoultefinder.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+    int n;
+
+    cout << "Enter value of n";
+    cin >> n ;
+
+    if (n<0)
+    {
+        n = 0 - n;
+
+        cout << n;
+    }
+
+    else
+    {
+        cout << n;
+    }
+
+}
