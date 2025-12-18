ba39013839be118896570d0d57207f385ce84695
Aditya Suthar <adityabishnoi576@gmail.com>
2025-12-18 17:31:46 +0530

    vectors size check

---
+++ C++ (DSA)/VECTORS(CH-02).cpp
@@ -0,0 +1,17 @@
+// FOR PRINTING SIZE OF VECTORS
+
+#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+int main ()
+
+{
+    vector <int> vec = {1,2,3,4,5,6,7,8};
+
+    cout << "The size of vector is:" << vec.size() << "\n";
+
+    return 0;
+}
