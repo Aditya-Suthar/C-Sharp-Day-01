38a0b53b9bea364f1b58658e383aa76aeceabdca
Aditya Suthar <adityabishnoi576@gmail.com>
2026-01-12 21:19:23 +0530

    python comparison

---
+++ PYTHON/Conditional1.py
@@ -0,0 +1,11 @@
+x = int (input("What's x ?"))
+y = int (input("What's y ?"))
+
+if (x>y):
+    print("x is greather")
+
+elif (x<y):
+    print ("y is greather")
+
+else:
+    print ("Both are equal")
