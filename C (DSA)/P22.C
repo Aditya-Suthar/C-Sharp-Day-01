0297eaa519ae93c56ad24dc42270e6502cdddbbd
Aditya Suthar <adityabishnoi576@gmail.com>
2026-01-14 18:56:17 +0530

    Swapping minimum and maximum value in the array

--- C (DSA)/P22.C
+++ C (DSA)/P22.C
@@ -1,6 +1,10 @@
+#include <stdio.h>
+
 int EleCount();
 void ScanArray (int arr[],int n);
 int SmallNumber(int arr[],int n);
+int LargeNumber(int arr[],int n);
+void SwitchLS(int arr[],int n,int k,int l);
 void DisplayArray(int arr[],int n);
 
 
@@ -8,15 +12,11 @@ int main ()
 {
 
     int n = EleCount();
-
     int arr[n];
-
     ScanArray(arr,n);
-
     int k = SmallNumber(arr,n);
-
-    printf("%d",k);
-
+    int l = LargeNumber(arr,n);
+    SwitchLS(arr,n,k,l);
     return 0;
 
 }
@@ -61,5 +61,51 @@ int SmallNumber(int arr[],int n)
     return temp;
 }
 
+int LargeNumber (int arr[],int n)
 
+{
+    int j;
+
+    int temp1 = arr[0];
+
+    for (j=0;j<n;j++)
+    {
+        if (arr[j] > temp1 )
+        {
+            temp1 = arr[j];
+        }
+    }
+
+    return temp1;
+}
+
+
+void SwitchLS(int arr[],int n,int k,int l)
+{
+    int i = 0;
+    int j = 0;
+    int temp;
+
+    for (int i=0;i<n;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            if (arr[i] == l && arr[j] == k)
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf ("\n New array is:-\n");
+
+    for (int k=0;k<n;k++)
+    {
+        printf ("a[%d]=%d\n",k,arr[k]);
+    }
+
+
+    }
 
--- PYTHON/hogwarts.py
+++
@@ -1,4 +0,0 @@
-students = ["Hermoine","Harry","Ron"]
-
-for student in students:
-    print (student)
