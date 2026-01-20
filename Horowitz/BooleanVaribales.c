3d9d35424b05a0fdfd6e24e797dbbc736a3fee07
Aditya Suthar <adityabishnoi576@gmail.com>
2026-01-20 21:43:00 +0530

    Selection Sort

---
+++ Horowitz/BooleanVaribales.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main ()
+
+{
+    if (0)
+    {
+        printf ("True");
+    }
+
+    else
+    {
+        printf ("False");
+    }
+
+    if (5)
+    {
+        printf ("True");
+    }
+
+    else 
+    {
+        printf ("False");
+    }
+}
