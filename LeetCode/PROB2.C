3e742a7d1b218e7068e2f87b8ed82f1a642c684e
Aditya Suthar <adityabishnoi576@gmail.com>
2026-01-29 22:39:23 +0530

    brute force PROB-2 Leetcode

---
+++ LeetCode/PROB2.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main ()
+
+{
+    int n;
+    
+    printf ("Enter the number of elements in the array:-");
+    scanf ("%d",&n);
+
+    int arr[n];
+
+    for (int i=0;i<n;i++)
+    {
+        printf ("Enter the value of elements in the arr[%d]:-",i);
+        scanf ("%d",&arr[i]);
+    }
+
+    int target;
+    printf ("Enter the value of target:-");
+    scanf ("%d",&target);
+
+    for (int i=0;i<n;i++)
+    {
+        for (int j=i+1;j<n;j++)
+        {
+            if (arr[i]+arr[j] == target)
+            {
+                printf ("%d %d",i,j);
+            }
+        }
+    }
+}
