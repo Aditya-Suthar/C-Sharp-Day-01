9789d400dfd93f2080f280fd422a388c06666839
Aditya Suthar <adityabishnoi576@gmail.com>
2026-01-31 11:15:05 +0530

    COUNTING REPEATING ELEMENTS INSIDE THE STRING

---
+++ HOD's WORK/Worksheet 2/STRING BASED/PROB1(B).c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+
+{
+    char str[100];
+    int count[256];
+
+    for (int i=0;i<256;i++)
+    {
+        count[i] = 0;
+    }
+
+    printf ("ENTER THE STRING:-");
+    scanf (" %[^\n]",str);
+
+    int len = strlen(str);
+
+    for (int i=0;i<len;i++)
+    {
+        ++count[str[i]];
+    }
+
+    for (int j=0;j<256;j++)
+    {
+        if (count[j]>0)
+        {
+            int max = count[j];
+            
+            printf ("%c has appeared %d times\n",j,count[j]);
+
+            
+        }
+    }
+
+    return 0;
+}
