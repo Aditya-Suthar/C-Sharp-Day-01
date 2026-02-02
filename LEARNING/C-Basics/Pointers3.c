ddfb9cf3d04bb57645780fbb91318128ec180f32
Aditya Suthar <adityabishnoi576@gmail.com>
2026-02-02 12:44:54 +0530

    counting the length of string using pointers

---
+++ LEARNING/C-Basics/Pointers3.c
@@ -0,0 +1,24 @@
+// Write a C program to find the length of a string using pointers only.
+
+#include <stdio.h>
+int main ()
+
+{
+    char str[50];
+
+    printf ("Enter the value of string:-");
+    scanf (" %[^\n]",str);
+
+    char *ptr;
+    ptr = &str[0];
+
+    int count = 0;
+
+    while (*ptr != '\0')
+    {
+        count ++;
+        ptr++;
+    }
+
+    printf ("The length of string is:- %d",count);
+}
