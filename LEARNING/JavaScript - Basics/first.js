69a6f104c7b03e27da8250882ab6e2ddaf50fb5a
Aditya Suthar <adityabishnoi576@gmail.com>
2026-02-10 23:29:11 +0530

    Java script changes

--- LEARNING/JavaScript - Basics/first.js
+++ LEARNING/JavaScript - Basics/first.js
@@ -2,3 +2,11 @@ console.log("HELLO");
 console.log("THIS");
 console.log("IS");
 console.log("JAVASCRIPT");
+
+fullname = "HELLO";
+x = null;
+y = undefined;
+
+console.log(fullname);
+console.log(x);
+console.log(y);
